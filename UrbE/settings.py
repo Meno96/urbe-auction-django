a78e7fcb39aea5962b29d674cd71aed94966a61f
Daniele <danielemeno96@gmail.com>
2023-03-25 12:48:45 +0100

    settings

--- UrbE/settings.py
+++ UrbE/settings.py
@@ -115,12 +115,12 @@ CACHES = {
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     },
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+}
 
 CORS_ORIGIN_ALLOW_ALL = False
 
